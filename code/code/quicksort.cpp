317466fb0b12b741fb0115218eab7911701959cf
Lei.Gao <Lei.Gao@igt.com>
2018-07-12 17:58:33 +0800

    first check in

---
+++ code/code/quicksort.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+#include <gtest\gtest.h>
+struct Interval {
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
+
+
+
+
