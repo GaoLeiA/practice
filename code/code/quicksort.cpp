d6cd2a8a69ea6784187ce0d6f7accbce25bc8f67
Lei.Gao <lei.gao@outlook.com>
2018-07-12 18:16:59 +0800

    remove unused

--- code/code/quicksort.cpp
+++ code/code/quicksort.cpp
@@ -1,11 +1,6 @@
 #include "Header.h"
 #include <gtest\gtest.h>
-struct Interval {
-	int start;
-	int end;
-	Interval() : start(0), end(0) {}
-	Interval(int s, int e) : start(s), end(e) {}
-};
+
 
 
 
