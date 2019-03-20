8f8e5310af90230026a8a40aea2285d6f707be5c
Lei.Gao <lei.gao@outlook.com>
2019-03-20 17:35:58 +0800

    new change for 2019 preview

--- code/code/testmain.cpp
+++ code/code/testmain.cpp
@@ -10,39 +10,10 @@ namespace testing {
 }  // namespace testing
 using testing::GTEST_FLAG(filter);
 
-/*
-
-Summarising @Rasmi Ranjan Nayak and @nogard answers and adding another option:
-
-On the console
-You should use the flag --gtest_filter, like
-
---gtest_filter=Test_Cases1*
-(You can also do this in Properties|Configuration Properties|Debugging|Command Arguments)
-
-On the environment
-You should set the variable GTEST_FILTER like
-
-export GTEST_FILTER = "Test_Cases1*"
-On the code
-You should set a flag filter, like
-
-::testing::GTEST_FLAG(filter) = "Test_Cases1*";
-such that your main function becomes something like
-
-int main(int argc, char **argv) {
-::testing::InitGoogleTest(&argc, argv);
-::testing::GTEST_FLAG(filter) = "Test_Cases1*";
-return RUN_ALL_TESTS();
-}
-See section Running a Subset of the Tests for more info on the syntax of the string you can use.
-*/
 int main(int argc, char **argv)
 {
-	//testing::GTEST_FLAG(filter) = "*";//":-:*Counter*";
-	//::testing::GTEST_FLAG(output) = "*";
-	//::testing::GTEST_FLAG(filter) = "*Counter*:*IsPrime*:*ListenersTest.DoesNotLeak*";//":-:*Counter*";
+
 	testing::InitGoogleTest(&argc, argv);
-	GTEST_FLAG(filter) = "Test_Cases1*";
+	//GTEST_FLAG(filter) = "Test_Cases1*";
 	return RUN_ALL_TESTS();
 }
---
+++ code/codeut/MyString.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "MyString.h"
+
+
+TEST(TestString, nametest) {
+
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+
+}
